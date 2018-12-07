3024012f35c8ed5d6b31a28fbedaae845e2081fe
Ahmad Alobaid <ahmad88me@gmail.com>
2018-12-07 09:41:07 +0100

    optimization test works

--- src/features.cpp
+++ src/features.cpp
@@ -12,11 +12,14 @@
 #include "filter_num.h" 
 #include "logger.h"
 
-static string logfname = "features.log";
-static string NUM_PROP_FNAME = "class_property_num.tsv";
-static string FEAT_FNAME = "features.tsv";
+//static string logfname = "features-opti-static.log";
+static string logfname = "features-opti-swap.log";
+//static string FEAT_FNAME = "features.tsv";
 //static string FEAT_FNAME = "features-min-height-BasketballPlayer.tsv";
-
+static string FEAT_FNAME = "features-opti-swap.tsv";
+//static string FEAT_FNAME = "features-opti-static.tsv";
+static bool SUBSET = false;
+static string rdf_type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 
 using namespace std;
 
@@ -85,10 +88,12 @@ double median(std::list<double>* values){
 
 // This is the entry point
 void write_features(string hdt_file_dir, string num_property_dir){
-    HDT *hdt = HDTManager::mapHDT(hdt_file_dir.c_str());       
+    //HDT *hdt = HDTManager::mapHDT(hdt_file_dir.c_str());
+    HDT *hdt = hdt::HDTManager::mapIndexedHDT(hdt_file_dir.c_str());
     ifstream in_file(num_property_dir);
     std::list<clspropair*>* processed=get_processed_feat_clspairs(FEAT_FNAME);
     std::list<string>* instances=nullptr;
+    std::list<string>* tem=nullptr;
     bool found, instances_fetched;
     string line,class_uri="";
     clspropair* pair;
@@ -101,10 +106,15 @@ void write_features(string hdt_file_dir, string num_property_dir){
             pairs = get_pairs_from_numfilter(line);
             class_uri = get_class_from_line(line);
             instances_fetched = false;
-            log(logfname, "("+to_string(pairs->size())+")"+" class: "+class_uri);
+            if(instances!=nullptr){
+                delete instances;
+            }
+            instances=nullptr;
+            log(logfname, "("+to_string(pairs->size())+") properties for "+" class: "+class_uri);
             for(auto it_p=pairs->cbegin();it_p!=pairs->cend();it_p++){
                 pair = (*it_p);
                 found = false;
+                //check if the pair is processed
                 for(auto it=processed->cbegin();it!=processed->cend();it++){
                     if((*it)->class_uri == pair->class_uri && (*it)->property_uri == pair->property_uri){
                         found = true;
@@ -112,18 +122,16 @@ void write_features(string hdt_file_dir, string num_property_dir){
                     }
                 }
                 if(!found){
-                    // if the instances are not for this class fetch them
-                    if(instances_fetched==false){
-                        if(instances!=nullptr){
-                            delete instances;
-                        }
-                        log(logfname,"fetching instances for "+class_uri);
-                        instances = get_instances(hdt, class_uri);
-                        instances_fetched = true;
-                        log(logfname,"num of instances: "+to_string(instances->size()));
+                    compute_store_features_for_pair(hdt, pair, &instances);
+                    if(instances==nullptr){
+                        log(logfname, "outside instances: null");
                     }
-                    log(logfname,"computing features for: "+pair->class_uri+" - "+pair->property_uri);
-                    compute_store_features_for_pair(hdt, instances, pair); 
+                    else{
+                        log(logfname, "outside instances: "+to_string(instances->size()));
+                    }
+//                    if(instances!=nullptr){
+//                        instances_fetched = true;
+//                    }
                     num_processed++;
                     log(logfname, "processed class/property pairs: "+to_string(num_processed));
                 }// if not found
@@ -137,9 +145,24 @@ void write_features(string hdt_file_dir, string num_property_dir){
     delete hdt;
 }
 
-void compute_store_features_for_pair(HDT* hdt, std::list<string>* instances, clspropair* pair){
+std::list<string>* get_subset(std::list<string> * l, int new_size){
+    std::list<string> * subs = new std::list<string>;
+    for (auto it = l->cbegin() ;  it != l->cend() && subs->size() < new_size; it++){
+        subs->push_back(*(it));
+    }
+    return subs;
+}
+
+
+void compute_store_features_for_pair(HDT* hdt, clspropair* pair, std::list<string>** instances_addr){
     string line;
-    line = compute_features_line(hdt, pair->class_uri, pair->property_uri, instances);
+    line = compute_features_line(hdt, pair->class_uri, pair->property_uri, instances_addr);
+    if((*instances_addr)==nullptr){
+        log(logfname, "intermediate: null ");
+    }
+    else{
+        log(logfname, "intermediate: "+to_string((*instances_addr)->size()));
+    }
     delete pair;
     //log(logfname, "writing the results");
     ofstream outf;
@@ -148,31 +171,115 @@ void compute_store_features_for_pair(HDT* hdt, std::list<string>* instances, cls
     outf.close();
 }
 
-string compute_features_line(HDT* hdt, string class_uri, string property_uri, std::list<string>* instances){
+string compute_features_line(HDT* hdt, string class_uri, string property_uri, std::list<string>** instances_addr){
     string line;
     std::list<double> *values = new std::list<double>;
     IteratorTripleString *itt;
     TripleString * triple;
     double v;
-    //log(logfname, "close look to the loop");
-    log(logfname, "num of instances: "+to_string(instances->size()));
-
-    for(auto it=instances->cbegin();it!=instances->cend();it++){
-        //log(logfname, "internal hdt search");
-        itt = hdt->search((*it).c_str(), property_uri.c_str(), "");
-        //log(logfname, "has next? "+to_string(itt->hasNext()));
-        if(itt->hasNext()){
+    size_t estimated_of_class, estimated_of_property;
+    itt = hdt->search("", property_uri.c_str(), "");
+    estimated_of_property = itt->estimatedNumResults();
+    delete itt;
+    itt = hdt->search("", rdf_type.c_str(), class_uri.c_str());
+    estimated_of_class = itt->estimatedNumResults();
+    delete itt;
+    std::list<string>* instances_of_class = new std::list<string>;//get_instances(hdt, class_uri); // instances of a given class
+    std::list<string>* instances_of_property = new std::list<string>;// instances that has a given property
+    std::list<string>* instances = new std::list<string>; // a holder of the join of the two
+    log(logfname, "num of instances of the class <"+class_uri+">: "+to_string(estimated_of_class));
+    log(logfname, "num of instances that has the property <"+property_uri+">: "+to_string(estimated_of_property));
+    //if(false){
+    if(estimated_of_property < estimated_of_class){
+        log(logfname, "SWAP!");
+        log(logfname, "Getting the instance for the property: "+property_uri);
+        // get instances of a given property
+        itt = hdt->search("", property_uri.c_str(), "");
+        while(itt->hasNext()){
             triple = itt->next();
-            if(str_to_double(triple->getObject(),v)){
-                values->push_front(v);
-            }
+            instances_of_property->push_back(triple->getSubject());
         }
         delete itt;
+        // get instances that are of the type class_uri
+        for(auto it=instances_of_property->cbegin();it!=instances_of_property->cend();it++){
+            itt = hdt->search((*it).c_str(), rdf_type.c_str(), "");
+            if(itt->hasNext()){
+                instances->push_back(*it);
+            }
+            delete itt;
+        }
+        // get the objects of the correct instances
+        for(auto it=instances->cbegin();it!=instances->cend();it++){
+            //log(logfname, "internal hdt search");
+            itt = hdt->search((*it).c_str(), property_uri.c_str(), "");
+            //log(logfname, "has next? "+to_string(itt->hasNext()));
+            if(itt->hasNext()){
+                triple = itt->next();
+                if(str_to_double(triple->getObject(),v)){
+                    values->push_front(v);
+                }
+            }
+            delete itt;
+        }
     }
+    else{
+        log(logfname, "No swap!");
+        // if instances of a given class is already fetched before
+        if((*instances_addr)!=nullptr){
+            log(logfname, "use cached instances");
+            instances_of_class = (*instances_addr);
+        }
+        else{
+            log(logfname, "fetching instances for a the class: "+class_uri);
+            // get the entities for a given class
+            itt = hdt->search("", rdf_type.c_str(), class_uri.c_str());
+            while(itt->hasNext()){
+                triple = itt->next();
+                instances_of_class->push_back(triple->getSubject());
+            }
+            delete itt;
+            (*instances_addr) = instances_of_class;
+            //instances_addr = &instances_of_class;
+            if((*instances_addr)==nullptr){
+                log(logfname, "ERROR??");
+            }
+            else{
+                log(logfname, "locally ok: "+to_string((*instances_addr)->size()));
+            }
+        }
+        // get the objects for the class/property pairs
+        for(auto it=instances_of_class->cbegin();it!=instances_of_class->cend();it++){
+            //log(logfname, "internal hdt search");
+            itt = hdt->search((*it).c_str(), property_uri.c_str(), "");
+            //log(logfname, "has next? "+to_string(itt->hasNext()));
+            if(itt->hasNext()){
+                triple = itt->next();
+                if(str_to_double(triple->getObject(),v)){
+                    values->push_front(v);
+                }
+            }
+            delete itt;
+        }
+    }
+//    for(auto it=instances->cbegin();it!=instances->cend();it++){
+//        //log(logfname, "internal hdt search");
+//        itt = hdt->search((*it).c_str(), property_uri.c_str(), "");
+//        //log(logfname, "has next? "+to_string(itt->hasNext()));
+//        if(itt->hasNext()){
+//            triple = itt->next();
+//            if(str_to_double(triple->getObject(),v)){
+//                values->push_front(v);
+//            }
+//        }
+//        delete itt;
+//    }
     double mean_value;
     mean_value = mean(values);
     line = class_uri + "\t" + property_uri +"\t"+ to_string(values->size())  + "\t" + to_string(mean_value) + "\t" + to_string(median(values)) + "\t" + to_string(stdev(values, mean_value));
     delete values;
+    delete instances;
+    //delete instances_of_class;
+    delete instances_of_property;
     return line+"\n";
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -13,18 +13,37 @@ using namespace std;
 using namespace hdt;
 
 
-void try_filter_class_property(string hdt_fname, string class_uri, string property_uri){
-    HDT *hdt = HDTManager::mapHDT(hdt_fname.c_str());
-    std::list<string>* instances=nullptr;
-    instances = get_instances(hdt, class_uri);
-    string line = compute_features_line(hdt, class_uri, property_uri, instances);
-    cout << "line: "<<line<<endl;
-}
+//void try_filter_class_property(string hdt_fname, string class_uri, string property_uri){
+//    HDT *hdt = HDTManager::mapHDT(hdt_fname.c_str());
+//    std::list<string>* instances=nullptr;
+//    instances = get_instances(hdt, class_uri);
+//    string line = compute_features_line(hdt, class_uri, property_uri, instances);
+//    cout << "line: "<<line<<endl;
+//}
 
+// Commit this to focus on the optimization
+//int main(){ahm
+//    string HDT_FNAME = "dbpedia_all.hdt";
+//    string CLASS_PROP_FNAME = "class_property_all.tsv";
+//    string NUM_PROP_FNAME = "class_property_num.tsv";
+//    //comment this line to speedup, but should be uncommented if numerical properties are not extracted yet.
+//    //store_num_cols(HDT_FNAME, CLASS_PROP_FNAME);
+//    write_features(HDT_FNAME, NUM_PROP_FNAME);
+//    //try_filter_class_property(HDT_FNAME, "http://dbpedia.org/ontology/BasketballPlayer", "http://dbpedia.org/ontology/Person/height");
+//    cout << "\nDone\n";
+//    return 0;
+//}
+
+/*
+ * This one is testing with the optimized approach, which is implemented in the features.cpp using the estimated number
+ * of results
+
+*/
 int main(){
     string HDT_FNAME = "dbpedia_all.hdt";
-    string CLASS_PROP_FNAME = "class_property_all.tsv";
-    string NUM_PROP_FNAME = "class_property_num.tsv";
+    //string NUM_PROP_FNAME = "class_property_num-opti.tsv";
+    string NUM_PROP_FNAME = "class_property_num-opti-swap.tsv";
+    //string NUM_PROP_FNAME = "class_property_num-opti-static.tsv";
     //comment this line to speedup, but should be uncommented if numerical properties are not extracted yet.
     //store_num_cols(HDT_FNAME, CLASS_PROP_FNAME);
     write_features(HDT_FNAME, NUM_PROP_FNAME);
@@ -34,4 +53,40 @@ int main(){
 }
 
 
+//int main(){
+//    string property_uri = "x:x";
+//    string class_uri = "http://www.w3.org/2003/01/geo/wgs84_pos#SpatialThing";
+//    //string HDT_FNAME = "dbpedia_all.hdt";
+//    string HDT_FNAME = "sample.hdt";
+//    string rdf_type = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+//    HDT *hdt = HDTManager::mapHDT(HDT_FNAME.c_str());
+//    cout << "\nnumber of entities for property <"<< property_uri << ">: "<< hdt->search("", property_uri.c_str(), "")->estimatedNumResults();
+//    auto itt = hdt->search("", property_uri.c_str(), "");
+//    long i;
+//    while(itt->hasNext()){
+//        i++;
+//        itt->next();
+//    }
+//    cout << "\niterationts: "<<i <<endl;
+//    cout << "\nnumber of entities for class: <"<< class_uri << ">: "<< hdt->search("",rdf_type.c_str() , class_uri.c_str())->estimatedNumResults();
+//    cout << "\nDone\n";
+//    return 0;
+//}
 
+// This is used to test the sample with estimatedNumResults
+//int main(){
+//    string property_uri = "x:x";
+//    string HDT_FNAME = "sample.hdt";
+//    //HDT *hdt = HDTManager::mapHDT(HDT_FNAME.c_str());
+//    HDT *hdt = hdt::HDTManager::mapIndexedHDT(HDT_FNAME.c_str());
+//    cout << "\nnumber of entities for property <"<< property_uri << ">: "<< hdt->search("", property_uri.c_str(), "")->estimatedNumResults();
+//    auto itt = hdt->search("", property_uri.c_str(), "");
+//    long i;
+//    while(itt->hasNext()){
+//        i++;
+//        itt->next();
+//    }
+//    cout << "\niterationts: "<<i <<endl;
+//    cout << "\nDone\n";
+//    return 0;
+//}
