11d6488556f5ec995ce762f91fe087ce9384c7ff
Ahmad Alobaid <ahmad88me@gmail.com>
2019-01-03 16:38:43 +0100

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,10 @@ int main(){
     string NUM_PROP_FNAME = "class_property_num.tsv";
     //comment this line to speedup, but should be uncommented if numerical properties are not extracted yet.
     //store_num_cols(HDT_FNAME, CLASS_PROP_FNAME);
-    write_features(HDT_FNAME, NUM_PROP_FNAME);
+//    write_features(HDT_FNAME, NUM_PROP_FNAME);
     //try_filter_class_property(HDT_FNAME, "http://dbpedia.org/ontology/BasketballPlayer", "http://dbpedia.org/ontology/Person/height");
+    Features feat(HDT_FNAME, "main.log");
+    feat.features_fname = "features.tsv";
     cout << "\nDone\n";
     return 0;
 }
